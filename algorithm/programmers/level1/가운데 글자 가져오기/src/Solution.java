0f6eb2e61a7eb052cdfb7ea9f18fa9a51233ea6a
junyeongjang <junyeong.dev@gmail.com>
2021-03-24 10:51:07 +0900

    프로그래머스 레벨2

--- algorithm/programmers/level1/가운데 글자 가져오기/src/Solution.java
+++ algorithm/programmers/level1/가운데 글자 가져오기/src/Solution.java
@@ -13,4 +13,20 @@ class Solution {
             answer = str.substring(str.length() / 2 - 1, str.length()/2 + 1);
         return answer;
     }
+
+    public long solution(int a, int b) {
+        long answer = 0;
+     if(a>b) {
+         int temp;
+         temp = a;
+         a= b;
+         b = temp;
+     }
+
+            for (int i = a; i <= b; i++) {
+                answer += i;
+            }
+
+        return answer;
+    }
 }
