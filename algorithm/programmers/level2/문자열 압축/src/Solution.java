3be9565aa33e0bb2341e9cad9ad076099ca217ee
junyeongjang <junyeong.dev@gmail.com>
2021-03-29 23:27:22 +0900

    Update Solution.java

--- algorithm/programmers/level2/문자열 압축/src/Solution.java
+++ algorithm/programmers/level2/문자열 압축/src/Solution.java
@@ -1,2 +1,16 @@
-public class Solution {
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        for(int i=0;i<s.length();i++){
+            String temp ="";
+            for(int j=0;j<s.length();j++) {
+                s.substring(j, j +i);
+                for(int k=j; k<s.length();k++){
+
+                }
+            }
+        }
+        return answer;
+    }
 }
