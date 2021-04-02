06ecc6c15806c0f3cdaa82ab3eb17330a3ded0f6
junyeongjang <junyeong.dev@gmail.com>
2021-04-02 23:35:15 +0900

    레벨2

--- algorithm/programmers/level2/문자열 압축/src/Solution.java
+++ algorithm/programmers/level2/문자열 압축/src/Solution.java
@@ -1,4 +1,7 @@
 import java.util.*;
+import java.util.*;
+import java.io.*;
+
 class Solution {
     public int solution(String s) {
         int answer = 0;
@@ -12,5 +15,27 @@ class Solution {
             }
         }
         return answer;
+    }
+}
+
+
+class Main
+{
+    public static void main(String args[])
+    {
+        long P, N;
+        long res = 0;
+        ArrayList<Long> arr = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        P = sc.nextLong();
+        N = sc.nextLong();
+        for(int i=0;i<N;i++){
+            long num = sc.nextLong();
+            arr.add(num);
+        }
+        for(int i=0;i<N;i++){
+            res += arr.get(i) * P;
+        }
+        System.out.print(res % 1000000007);
     }
 }
