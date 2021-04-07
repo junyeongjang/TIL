cdb15495f16798601fe371c278eceb27493581c5
junyeongjang <junyeong.dev@gmail.com>
2021-04-07 19:45:02 +0900

    spring 2일차

---
+++ Spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,19 @@
+package hello.hellospring.domain;
+
+public class Member {
+    private Long id;
+    private String Name;
+
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+}
