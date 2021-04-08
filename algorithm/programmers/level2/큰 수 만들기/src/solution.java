ce29f6dc63ddf83c6ce091082404031b6eb1bff8
junyeongjang <junyeong.dev@gmail.com>
2021-04-08 20:16:22 +0900

    Update solution.java

--- algorithm/programmers/level2/큰 수 만들기/src/solution.java
+++ algorithm/programmers/level2/큰 수 만들기/src/solution.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public int solution(int[] citations) {
-        int answer;
+        int answer = 0;
         Arrays.sort(citations);
         for (int i = 0; i < citations.length; i++) {
             int h = citations.length - i;
